225c2f539a9cd17e6bfacebf99ce2ca3a8e9d6d0
rejdeboer <rick.deboer@live.nl>
2024-07-05 12:30:23 +0200

    feat: ec2 keypair

---
+++ terraform/pki.tf
@@ -0,0 +1,14 @@
+resource "aws_key_pair" "cluster_key" {
+  key_name   = "cluster-key"
+  public_key = tls_private_key.cluster_key.public_key_openssh
+}
+
+resource "tls_private_key" "cluster_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "cluster_key" {
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "${aws_key_pair.cluster_key.key_name}.pem"
+}
